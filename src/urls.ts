5b34399ce21220f9ebf1712744d89fc84c0ee547
Marc Durdin <marc@durdin.net>
2024-04-17 05:19:06 +0700

    chore: comments

--- src/urls.ts
+++ src/urls.ts
@@ -5,19 +5,22 @@ type RedirectFunction = (matches: RegExpMatchArray) => string;
 interface ShortURL { match: string|RegExp; redirect: string|RedirectFunction };
 
 export const shortURLs: ShortURL[] = [
+  // Keyman Developer compiler error short codes KM#####, e.g. KM02010, km02010
   { match: /^(km[0-9a-f]{5})$/i, redirect: km },
-	{ match: /^kb(\d+)$/i, redirect: kb },
 
-	/**
-	 * Conferences and presentations, starting in 2024
-	 */
+  // Knowledge Base articles, e.g. kb00040, kb40, KB40
+  { match: /^kb(\d+)$/i, redirect: kb },
 
-	// Keyman Roadmap presentation, March 2024 LTCon
-	{ match: "ltcon2024", redirect: "https://docs.google.com/presentation/d/1PvhQTA3KkQoddp_opqsJkP6cCGXDE8cP8AiIvW27Bac/edit?usp=sharing"},
-	// Keyman LDML Keyboards presentation, March 2024 LTCon
-	{ match: "ltcon2024ldml", redirect: "https://docs.google.com/presentation/d/1UBjNHdEdiiTb33H6AfqRtJqOXtl9ZBwg20ZMW7illZU/edit?usp=sharing"},
-	// Keyman What's New presentation, April 2024 LTCT
-	{ match: "ltct2024", redirect: "https://docs.google.com/presentation/d/1lQlbvxA0w_In9lq7heHYBqkK0h06p-UjMBwpFv9_IZA/edit?usp=sharing"},
+  /*
+   * Conferences and presentations, starting in 2024
+   */
 
-	// { match: "submit-keyboard", redirect: "https://help.keyman.com/developer/keyboards/" },
+  // Keyman Roadmap presentation, March 2024 LTCon
+  { match: "ltcon2024", redirect: "https://docs.google.com/presentation/d/1PvhQTA3KkQoddp_opqsJkP6cCGXDE8cP8AiIvW27Bac/edit?usp=sharing"},
+  // Keyman LDML Keyboards presentation, March 2024 LTCon
+  { match: "ltcon2024ldml", redirect: "https://docs.google.com/presentation/d/1UBjNHdEdiiTb33H6AfqRtJqOXtl9ZBwg20ZMW7illZU/edit?usp=sharing"},
+  // Keyman What's New presentation, April 2024 LTCT
+  { match: "ltct2024", redirect: "https://docs.google.com/presentation/d/1lQlbvxA0w_In9lq7heHYBqkK0h06p-UjMBwpFv9_IZA/edit?usp=sharing"},
+
+  // { match: "submit-keyboard", redirect: "https://help.keyman.com/developer/keyboards/" },
 ];
